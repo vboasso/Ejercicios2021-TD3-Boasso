8ff74aed825d9ecbe49dd07b043089a5b2c7eb54
Valentín <vale_boasso@hotmail.com>
2021-05-10 18:23:07 -0300

    Ej17 Aumento stak tarea destello

--- Ej17-TPulsadorPeriodico/src/main.c
+++ Ej17-TPulsadorPeriodico/src/main.c
@@ -3,7 +3,7 @@
 //Variables globales
 int checkup=0;
 int checkdown=0;
-uint16_t duty = 0;
+uint32_t duty = 0;
 TickType_t conteoTicksInicio = 0;
 TickType_t conteoTicksFinal = 0;
 
@@ -15,7 +15,7 @@ void app_main() {
     BaseType_t res1 = xTaskCreatePinnedToCore(
     	TareaDestellar,                     	// Funcion de la tarea a ejecutar
         "Tarea led destellando",   	                // Nombre de la tarea como String amigable para el usuario
-        configMINIMAL_STACK_SIZE, 		// Cantidad de stack de la tarea
+        configMINIMAL_STACK_SIZE*3, 		// Cantidad de stack de la tarea
         NULL,                          	// Parametros de tarea
         tskIDLE_PRIORITY+1,         	// Prioridad de la tarea -> Queremos que este un nivel encima de IDLE
         NULL,                          		// Puntero a la tarea creada en el sistema
